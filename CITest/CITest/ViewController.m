0d69525e6562b14bf2123a064b7c8f037d011294
Tongtong Xu <xutongtong@breadtrip.com>
2015-02-12 15:50:18 +0800

    init

---
+++ CITest/CITest/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  CITest
+//
+//  Created by Tongtong Xu on 15/2/12.
+//  Copyright (c) 2015年 xxx Innovation Co. Ltd. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
